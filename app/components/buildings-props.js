f485d61960676e89434cc34828a7faa3ffdfc1c3
Costa <costa.mail@ya.ru>
2016-06-21 10:44:51 +0300

    ok

--- app/components/buildings-props.js
+++ app/components/buildings-props.js
@@ -4,6 +4,9 @@ export default Ember.Component.extend({
   pares: Em.computed('model.building.props', function(){
     let i,j,temparray,chunk = 2, result = [];
     let array = this.get('model.building.props');
+    if(!array){
+      return [];
+    }
     for (i=0,j=array.length; i<j; i+=chunk) {
       temparray = array.slice(i,i+chunk);
       result.push(temparray);
